a4687eb89a2f3e3a17ca28743f73ae0c169a42ce
motemen <motemen@gmail.com>
2013-09-27 23:13:41 +0900

    simple implementation

--- lib/humr/splitter/default.rb
+++ lib/humr/splitter/default.rb
@@ -1,33 +1,20 @@
-require 'strscan'
-
 module Humr
   module Splitter
     class Default
       Impl[:default] = self
 
+      PATTERN = /
+        (?<s>")  (?<field>.*?) (?<e>") |
+        (?<s>\[) (?<field>.*?) (?<e>\]) |
+        (?<field>\S+)
+      /x
+
       def sub_each_field(line, &block)
-        scanner = StringScanner.new(line)
-
-        result = ''
-        index = 1
-
-        loop do
-          if s = scanner.scan(/".*?"|\[.*?\]/)
-            result << s[0]
-            result << yield(s[1..-2], index)
-            result << s[-1]
-            index += 1
-          elsif s = scanner.scan(/\S+/)
-            result << yield(s, index)
-            index += 1
-          elsif not scanner.eos?
-            result << scanner.scan(/\s*/)
-          else
-            break
-          end
+        index = 0
+        line.gsub(PATTERN) do |m|
+          index += 1
+          [ $~['s'], yield($~['field'], index), $~['e'] ].join
         end
-
-        result
       end
     end
   end
