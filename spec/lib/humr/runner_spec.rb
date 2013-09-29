d564402b82163d6ecab55843302768db09250967
motemen <motemen@gmail.com>
2013-09-29 22:33:16 +0900

    do not depend on TZ

--- spec/lib/humr/runner_spec.rb
+++ spec/lib/humr/runner_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'spec_helper'
 require 'humr'
+require 'time'
 
 describe Humr::Runner do
   subject { Humr::Runner.new(nil) }
@@ -10,7 +11,7 @@ describe Humr::Runner do
       expect(subject.readable_line(<<-INPUT)).to eq(<<-READABLE)
         66.249.66.33 - - [08/Sep/2013:04:33:45 +0900] "GET /randompoku/?%E3%82%AF%E3%83%83%E3%82%AF%E3%83%91%E3%83%83%E3%83%89 HTTP/1.1" 200 4710 "-" "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
       INPUT
-        66.249.66.33 - - [#{'2013-09-08 04:33:45 +0900'.color(:yellow)}] "GET /randompoku/?#{'クックパッド'.color(:green)} HTTP/1.1" 200 #{'4.7k'.color(:cyan)} "-" "#{'Googlebot/2.1'.color(:magenta)}"
+        66.249.66.33 - - [#{Time.parse('2013-09-08 04:33:45 +0900').to_s.color(:yellow)}] "GET /randompoku/?#{'クックパッド'.color(:green)} HTTP/1.1" 200 #{'4.7k'.color(:cyan)} "-" "#{'Googlebot/2.1'.color(:magenta)}"
       READABLE
     end
   end
